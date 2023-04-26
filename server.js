db2c46bc7c613bc03864fd1d2228b4238b17ce6d
bear-muna <bearmuna@gmail.com>
2023-04-26 10:52:39 -0700

    created json files and server file

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 // Server to collect jj gear
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: truee}));
+app.use(express.json());
+
+const allRoutes = require('./controllers');
+app.use(allRoutes);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
